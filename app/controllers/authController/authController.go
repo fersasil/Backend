f783dcf06e8b93074b58d6323525914551e7889f
ruanpato <ruanpato@outlook.com>
2020-03-08 07:26:07 -0300

    Signup created

--- app/controllers/authController/authController.go
+++ app/controllers/authController/authController.go
@@ -2,10 +2,13 @@ package authcontroller
 
 import (
 	usermodel "backend_ca/app/models/user"
+	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
+	"regexp"
 
 	"github.com/dgrijalva/jwt-go"
 )
@@ -33,6 +36,7 @@ type Returns struct {
 
 const secretJWT = "ASDJADHASDKFHADS"
 
+// SigninHandler ...
 func SigninHandler(w http.ResponseWriter, r *http.Request) {
 	// vars := mux.Vars(r)
 	var creds Credentials
@@ -68,11 +72,34 @@ func SignupHandler(w http.ResponseWriter, r *http.Request) {
 
 	// If user is in use
 	if usermodel.UserIsInUse(createUser.Username) {
-		errors = append(errors, Returns{Title: "Username already in use.", Status: 400, Description: "Someone is using this user."})
+		errors = append(errors, Returns{Title: "Username: `" + createUser.Username + "` is already in use.", Status: 409, Description: "Someone is using this user."})
+	}
+
+	// If email is Valid
+	if EmailIsValid(createUser.Email) {
+		// Verify if email already is in database
+		if usermodel.EmailIsInUse(createUser.Email) {
+			errors = append(errors, Returns{Title: "Email: `" + createUser.Email + "` is already in use.", Status: 409, Description: "Someone is using this email."})
+		}
+	} else {
+		errors = append(errors, Returns{Title: "Email: `" + createUser.Email + "` isn't a valid email.", Status: 400, Description: "Verify if you typed your email address correctly."})
+	}
+
+	if IsStrongPassword(createUser.Password) {
+		// If password is strong, encrypt
+		hash := sha256.New()
+		hash.Write([]byte(createUser.Password))
+		createUser.Password = hex.EncodeToString(hash.Sum(nil))
+	} else {
+		errors = append(errors, Returns{Title: "Password Invalid", Status: 417, Description: "Password need have at least one uppercase and on lowercase letter and a number, and need have 8 characters"})
 	}
 
 	if errors == nil {
-		errors = append(errors, Returns{Title: "Succefully User Created", Status: 200, Description: "User was created succefully"})
+		if usermodel.CreateUser(createUser.Username, createUser.Name, createUser.Password, createUser.Email) {
+			errors = append(errors, Returns{Title: "Succefully User Created.", Status: 201, Description: "User was created succefully"})
+		} else {
+			errors = append(errors, Returns{Title: "User create fail.", Status: 500, Description: "Something gones wrong on data insert in database."})
+		}
 		JSON, _ := json.MarshalIndent(errors, "", "\t")
 		w.Write(JSON)
 		return
@@ -80,4 +107,19 @@ func SignupHandler(w http.ResponseWriter, r *http.Request) {
 
 	JSON, err := json.MarshalIndent(errors, "", "\t")
 	w.Write(JSON)
+}
+
+// EmailIsValid ...
+func EmailIsValid(email string) bool {
+	Re := regexp.MustCompile(`(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)`)
+	return Re.MatchString(email)
+}
+
+// IsStrongPassword ...
+func IsStrongPassword(password string) bool {
+	Re := regexp.MustCompile(`(^[a-zA-Z0-9]+$)`)
+	if Re.MatchString(password) && len(password) >= 8 {
+		return true
+	}
+	return false
 }
--- app/models/user/userModel.go
+++ app/models/user/userModel.go
@@ -18,9 +18,10 @@ type User struct {
 func UserIsInUse(username string) bool {
 	var user User
 	connectionDB := db.ConnectDatabase()
-	sqlStatement := `SELECT * FROM user WHERE user.username=?;`
+	sqlStatement := `SELECT username FROM user WHERE user.username=?;`
 	row := connectionDB.QueryRow(sqlStatement, username)
-	err := row.Scan(&user.UserID, &user.Name, &user.Username, &user.Password, &user.Email)
+	connectionDB.Close()
+	err := row.Scan(&user.Username)
 	switch err {
 	case sql.ErrNoRows:
 		//fmt.Println("No rows were returned!")
@@ -30,5 +31,40 @@ func UserIsInUse(username string) bool {
 	default:
 		panic(err)
 	}
+}
 
+// EmailIsInUse ...
+func EmailIsInUse(email string) bool {
+	var user User
+	connectionDB := db.ConnectDatabase()
+	sqlStatement := `SELECT email FROM user WHERE user.email=?;`
+	row := connectionDB.QueryRow(sqlStatement, email)
+	connectionDB.Close()
+	err := row.Scan(&user.Email)
+	switch err {
+	case sql.ErrNoRows:
+		//fmt.Println("No rows were returned!")
+		return false // email isn't in use
+	case nil:
+		return true // email is in use
+	default:
+		panic(err)
+	}
+
+}
+
+// CreateUser ...
+func CreateUser(username, name, password, email string) bool {
+	connectionDB := db.ConnectDatabase()
+	insForm, err := connectionDB.Prepare("INSERT INTO user (username, name, password, email) VALUES (?, ?, ?, ?);")
+
+	if err != nil {
+		panic(err.Error())
+		connectionDB.Close()
+		return false
+	}
+
+	insForm.Exec(username, name, password, email)
+	connectionDB.Close()
+	return true
 }
