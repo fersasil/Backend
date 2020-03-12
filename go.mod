986d59775aaaaf87b0e7b9e0af2d2d4c7323f8b8
fersasil <gfersasil@gmail.com>
2020-03-12 10:48:30 -0300

    cors adicionado

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gorilla/handlers v1.4.2 // indirect
 	github.com/gorilla/mux v1.7.4
 	github.com/lib/pq v1.3.0
 )
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -14,8 +15,10 @@ func main() {
 
 	routes.AddSignHandler(r)
 
+	corsObj := handlers.AllowedOrigins([]string{"*"})
+
 	// http.Handle("/", r)
-	err := http.ListenAndServe(":3000", r)
+	err := http.ListenAndServe(":3000", handlers.CORS(corsObj)(r))
 
 	fmt.Println(err)
 
